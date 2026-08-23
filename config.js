37a5e35c476be7231451067cc9e50d822cf5f190
maths <mr.nephews@gmail.com>
2026-08-23 22:17:34 +0200

    Add TMDB API key setup instructions to config.js
    Added instructions for obtaining TMDB API key.

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+// Enter your own API KEY - this is an example
+/* *******************************************
+Step 1: Get your own free TMDB API key (1 minute)
+Go to TheMovieDB.org and create a free account.
+Go to Settings -> API and request a Developer (Free) key.
 
+Copy your API Key (v3 auth).
+
+Step 2: Use the working code below
+Replace the code in your index.html with the script below. Change 'YOUR_API_KEY' on line 87 to the key you copied. 
+******************************************* */
+
+const CONFIG = {
+  TMDB_API_KEY: 'YOUR_API_KEY'
+};
